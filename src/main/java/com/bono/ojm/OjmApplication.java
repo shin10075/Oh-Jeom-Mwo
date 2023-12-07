680929724d76e4796c8b8e0fc86674411d40dbcb
shin10075 <shin10075@gmail.com>
2023-12-07 12:00:12 +0900

    connect to github

---
+++ src/main/java/com/bono/ojm/OjmApplication.java
@@ -0,0 +1,13 @@
+package com.bono.ojm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OjmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OjmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bono/ojm/OjmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bono.ojm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OjmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
