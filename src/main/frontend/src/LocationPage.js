72f532acd44958f87e6c11634e9cb35851c23893
shin10075 <shin10075@gmail.com>
2023-12-12 19:04:51 +0900

    Divide style file to individual

---
+++ src/main/frontend/src/LocationPage.js
@@ -0,0 +1,8 @@
+function LocationPage() {
+
+
+
+
+}
+
+export default LocationPage;
--- src/main/frontend/src/Main.js
+++ src/main/frontend/src/Main.js
@@ -1,12 +1,12 @@
-import "./App.css";
 import SearchLocation from "./SearchLocation";
+import styles from "./Main.module.css";
 
 function Main() {
 
     return (
-        <div className="main">
-            <div className="ojmLogo">
-                <img className="mainLogo" alt="ojmLogo" src="img/logo.png"/>
+        <div className={styles.main}>
+            <div className={styles.ojmLogo}>
+                <img className={styles.mainLogo} alt="ojmLogo" src="img/logo.png"/>
             </div>
             <SearchLocation/>
         </div>
